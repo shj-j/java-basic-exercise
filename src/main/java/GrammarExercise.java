c697f12b295385e074f33170a8f89360a43276e0
shj-j <shang.jie@outlook.com>
2020-07-27 16:04:35 +0800

    [sj]: done grammarexercise

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,39 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
     public static void main(String[] args) {
-        //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+        //
+        Scanner input = new Scanner( System.in );
+        String firstWordList = input.next();
+        String secondWordList = input.next();
+        input.close();
 
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
-        //按要求输出到命令行
+        //
+        System.out.println(result);
+
 
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
-        //在这编写实现代码
-        return null;
+        //
+        List<String> fistList = Arrays.asList(firstWordList.toUpperCase().split(","));
+        List<String> secondList = Arrays.asList(secondWordList.toUpperCase().split(","));
+
+        //check if input valid, invalid:throw error
+        if ( fistList.stream().filter(s-> ! s.matches("[a-zA-Z]+")).count() > 0 
+            || secondList.stream().filter(s-> ! s.matches("[a-zA-Z]+")).count() > 0 )
+            throw new RuntimeException("input not valid");
+        // change list to stream and remove the same words, 
+        // filter using to pick the words that contain in both list. 
+        // add the space between characters by using collector to change stream to string and replace "" to " "
+        String resString = fistList.stream().distinct().filter(s->secondList.contains(s))
+        .collect(Collectors.joining(",")).replace("", " ").replace(" , ", ",").trim();
+        // change string back to list and return result
+        List<String> resultList = Arrays.asList(resString.split(","));
+        return resultList;
     }
 }
